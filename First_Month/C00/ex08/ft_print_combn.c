557f2d3c591ec78c43bb72b9c7588a3749559ac7
Chan Yeu Kang <cyeu-kan@c2r1s2.42iskandarputeri.edu.my>
2024-09-30 19:20:12 +0800

    Done

--- First_Month/C00/ex08/ft_print_combn.c
+++ First_Month/C00/ex08/ft_print_combn.c
@@ -5,44 +5,44 @@ void ft_putchar(char c){
 }
 
 void ft_print_combn(int n){
-	if (n == 2){
-	for(int i = 0; i <= 8; i++){
-                for(int j = i+1; j <= 9; j++){
-		ft_putchar(i + '0');
-		ft_putchar(j + '0');
-                   if(!(i==8 && j==9)){
-			   ft_putchar(',');
-                           ft_putchar(' ');
-	}}}} else if (n==3){
-		for(int i = 0; i <= 7; i++){
-		for(int j = i+1; j <= 8; j++){
-			for(int k = j+1; k <= 9; k++){
-				ft_putchar(i + '0');
-				ft_putchar(j + '0');
-				ft_putchar(k + '0');
-				if(!(i==7 && j==8 && k==9)){
-					ft_putchar(',');
-					ft_putchar(' ');		
-}}}}}
-else if (n==4){
-	for(int i = 0; i <= 6; i++){
-                for(int j = i+1; j <= 7; j++){
-                        for(int k = j+1; k <= 8; k++){
-				for(int l = k+1; l<=9; l++){
+	int comb[10];
+	int i;
+	int j;
 
-                                ft_putchar(i + '0');
-                                ft_putchar(j + '0');
-                                ft_putchar(k + '0');
-				ft_putchar(l + '0');
+	for(i = 0; i < n; i++){
+		comb[i]=i;
+	}
 
-                                if(!(i==6 && j==7 && k==8 && l==9)){
-                                        ft_putchar(',');
-                                        ft_putchar(' ');
-}}}}}}}
+	while(1){
+		for(i = 0; i < n; i++){
+			ft_putchar(comb[i] + '0');
+		}
+
+	if(comb[0] == 10 - n){
+	        break;
+	}
+	        
+	ft_putchar(',');
+	ft_putchar(' ');
+	i = n - 1;
+	
+	while(i>=0 && comb[i] == 9 - (n -1 -i)){
+		i--;
+	}
+
+	if(i >=0){
+		comb[i]++;
+		for(j = i + 1; j < n; j++){
+			comb[j] = comb[j - 1] + 1;
+		}
+	}
+	}
+}
 
 int main(void){
-	ft_print_combn(4);
+	ft_print_combn(7);
 	ft_putchar('\n');
 	return 0;
 }
+
 
