2e0af6f79e62ee230963f6ec980262772e67dd0f
YKC0722 <ykchan0722@gmail.com>
2024-10-02 11:32:27 +0800

    revised

--- Correct_answer/C00/ex08/ft_print_combn.c
+++ Correct_answer/C00/ex08/ft_print_combn.c
@@ -1,49 +1,51 @@
-#include<unistd.h>
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_print_combn(int n)
+void    ft_print_combn(int n)
 {
-	int	comb[10];
-	int	i;
-	int	j;
+    int comb[10];
+    int i, j;
 
-	i = 0;
-	while (i < n)
-	{
-		comb[i] = i;
-		i++;
-		ft_putchar(comb[i] + '0');	
-	}
-
-	if(comb[0] == 10 - n){
-	       break ;
-	}
-	        
-	ft_putchar(',');
-	ft_putchar(' ');
+    i = 0;
+    while (i < n)
+    {
+        comb[i] = i;
+        i++;
+    }
+    while (comb[0] <= 10 - n)
+    {
+        i = 0;
+        while (i < n)
+		ft_putchar(comb[i++] + '0');
+	if (comb[0] < 10 - n)
+		write(1, ", ", 2);
 	i = n - 1;
-	
-	while(i>=0 && comb[i] == 9 - (n -1 -i)){
-		i--;
-	}
-
-	if(i >=0){
+        while (i >= 0 && comb[i] == 9 - (n - 1 - i))
+            i--;
+        if (i >= 0)
+        {
 		comb[i]++;
-		for(j = i + 1; j < n; j++){
-			comb[j] = comb[j - 1] + 1;
+		j = i + 1;
+		while (j < n)
+		{
+		comb[j] = comb[j - 1] + 1;
+		j++;
 		}
 	}
+	else
+	{
+		break;
+	}
+    }
 }
 
 int	main(void)
 {
-	ft_print_combn(7);
+	ft_print_combn(5);
 	ft_putchar('\n');
 	return (0);
 }
-
-
