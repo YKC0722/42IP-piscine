24461bfed532822ccb2851c21cfcacab0abfd890
YKC0722 <ykchan0722@gmail.com>
2024-10-08 19:34:26 +0800

    Update ft_putnbr.c

--- First_Month/C00/ex07/ft_putnbr.c
+++ First_Month/C00/ex07/ft_putnbr.c
@@ -27,12 +27,8 @@ void	ft_putnbr(int nb)
 	if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
 	}
-	else
-	{
-		ft_putchar((nb % 10) + '0');
-	}
+	ft_putchar((nb % 10) + '0');
 }
 
 /*int	main(void)
