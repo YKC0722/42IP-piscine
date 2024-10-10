ed9dc80f483e949221d7d9d49545f407bf2da14d
Chan Yeu Kang <cyeu-kan@c2r1s2.42iskandarputeri.edu.my>
2024-10-10 19:58:52 +0800

    C03_1

---
+++ First_Month/C03_1/ex01/ft_strcmp.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cyeu-kan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 17:35:05 by cyeu-kan          #+#    #+#             */
+/*   Updated: 2024/10/10 19:37:55 by cyeu-kan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+/*void	ft_putnbr(int n)
+{
+	char	c;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10);
+	}
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	char	s1[] = "ABC";
+	char	s2[] = "BAC";
+	int	result = ft_strcmp(s1, s2);
+
+	ft_putnbr(result);
+	return (0);
+}*/
